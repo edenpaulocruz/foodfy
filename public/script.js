c46e93fed35e84c5cca79be9b8d7e3314586bf60
edenpaulocruz <edenpaulo@gmail.com>
2022-02-24 01:50:06 -0300

    Refatora a exibição da página da receita
    Transforma o grid de exibição das receitas que é invocado na
    página inicial e na lista de receitas.
    
    Adiciona `id` ao carda da receita utilizando `loop.index0`.
    
    Simplifica função de chamada a página da receita utilizando o `id`.
    
    Altera o subtítulo Informações para Informações Adicionais na
    página da receita.

--- public/script.js
+++ public/script.js
@@ -2,13 +2,7 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
   card.addEventListener('click', function() {
-    const cardTitle = card.querySelector('.card__title').innerHTML
-    let cardIndex
-    cards.forEach((item, index) => {
-      if (item.querySelector('.card__title').innerHTML == cardTitle) {
-        return cardIndex = index
-      }
-    })
+    const cardIndex = card.id
     window.location.href = `/recipes/${cardIndex}`
   })
 }
