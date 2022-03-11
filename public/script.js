b032bc00e0e93b33c0f149fc0a2eec68cf245115
edenpaulocruz <edenpaulo@gmail.com>
2022-03-10 21:19:19 -0300

    Adiciona mostrar/esconder detalhes da receita

--- public/script.js
+++ public/script.js
@@ -6,3 +6,19 @@ for (let card of cards) {
     window.location.href = `/recipes/${cardIndex}`
   })
 }
+
+const toggles = document.querySelectorAll('.toggle')
+
+for (let toggle of toggles) {
+  toggle.addEventListener('click', function() {
+    const toggleSection = ((toggle.parentElement).parentElement)
+    const toggledContent = toggleSection.querySelector('.toggled-content')
+    if (toggledContent.style.display === '') {
+      toggledContent.style.display = 'none'
+      toggle.innerHTML = 'Mostrar'
+    } else {
+      toggledContent.style.display = ''
+      toggle.innerHTML = 'Esconder'
+    }
+  })
+}
